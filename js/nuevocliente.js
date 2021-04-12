c3c724141f38b3a412e788b1b8d672e4337c2f4f
Matias <matiasgarcia444@gmail.com>
2021-04-12 16:35:29 -0300

    comitaje

---
+++ js/nuevocliente.js
@@ -0,0 +1,62 @@
+(function(){
+    let DB
+    const formulario = document.querySelector('#formulario');
+    
+    document.addEventListener('DOMContentLoaded', () => {
+
+        conectarDB()
+        formulario.addEventListener('submit',validarCliente)
+    })
+
+    
+
+    function validarCliente(e){
+        e.preventDefault()
+
+        //Leer los inputs del formulario
+
+        const nombre = document.querySelector('#nombre').value
+        const email = document.querySelector('#email').value
+        const telefono = document.querySelector('#telefono').value
+        const empresa = document.querySelector('#empresa').value
+        
+        if (nombre === '' || email === '' || empresa === '' || telefono === ''){
+            imprimirAlerta('Todos los campos son obligatorios','error')
+            return
+        }
+
+        //Crear objeto
+        const cliente = {
+            nombre : nombre,
+            email : email,
+            telefono: telefono,
+            empresa: empresa,
+            
+        }
+        cliente.id = Date.now()
+        crearNuevoCliente(cliente)
+    }
+
+    function crearNuevoCliente(cliente){
+        const transaction = DB.transaction(['crm'],'readwrite')
+
+        const objectStore = transaction.objectStore('crm')
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function(){
+            imprimirAlerta('Hubo un error','error')
+        }
+
+        transaction.oncomplete = function(){
+            imprimirAlerta('El Cliente se agrego Correctamente')
+
+            setTimeout(()=>{
+                window.location.href = 'index.html'
+            },3000)
+        }
+
+    }
+
+    
+})()
